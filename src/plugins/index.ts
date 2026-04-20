6dd8cdb534ba3a3d868f97b23b12272d4745ca19
宋秀林 <songxiulin@163.com>
2026-04-20 16:26:34 +0900

    feat(state-persistence): add Pinia persisted state plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,7 @@
 // Types
 import type { App } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from '../router'
 /**
  * plugins/index.ts
@@ -13,7 +14,9 @@ import vuetify from './vuetify'
 
 export function registerPlugins(app: App) {
   app.use(vuetify)
-  app.use(createPinia())
+  const pinia = createPinia()
+  pinia.use(piniaPluginPersistedstate)
+  app.use(pinia)
   app.use(i18n)
   app.use(router)
 }
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -5,4 +5,5 @@ export const useAppStore = defineStore('app', {
   state: () => ({
     //
   }),
+  persist: true,
 })
